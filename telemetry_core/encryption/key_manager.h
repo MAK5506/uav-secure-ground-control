dc9019b6165b9aa79f881b51d5702235b94e298e
Muhammed Akif Korkmaz <47792384+MAK5506@users.noreply.github.com>
2026-02-13 01:48:43 +0300

    key_manager.h

--- telemetry_core/encryption/key_manager.h
+++ telemetry_core/encryption/key_manager.h
@@ -1,1 +1,10 @@
+#pragma once
+#include <vector>
+#include <cstdint>
+
+class KeyManager {
+public:
+    static std::vector<uint8_t> generateSessionKey();
+    static std::vector<uint8_t> generateIV();
+};
 
